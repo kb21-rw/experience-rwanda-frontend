ced4fd2e65a8d6b9db42797a879e4ddba0f02f00
Beccatoni <rbccmtni1@gmail.com>
2025-02-20 20:13:00 +0200

    chore: adjust the image to meet the design dimensions

--- src/app/components/ui/TripCard.tsx
+++ src/app/components/ui/TripCard.tsx
@@ -12,12 +12,12 @@ type Props = {
 export default function TripCard({image, location, price, date}: Props) {
   return (
     <div className='flex flex-col bg-white p-2 rounded-3xl gap-4'>
-    <Image src={image} alt={location} width={500} height={300} />
+    <Image src={image} alt={location} width={370} height={260} />
     <div className='flex flex-col gap-2 justify-start p-5'>
       <h3 className='font-bold'>Place: {location}</h3>
       <p>Price: {price}$/day</p>
       <p>Date: {date.toLocaleDateString()}</p>
-      <Link href={'#'} className='border border-black w-1/4 py-2 text-center rounded-lg'>More Details</Link>
+      <Link href={'#'} className='border border-black w-2/4 py-2 text-center rounded-lg'>More Details</Link>
       <button className='bg-black text-white rounded-full py-3 w-3/4'>Book Now</button>
     </div>
     </div>
