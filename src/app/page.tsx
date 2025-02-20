395982feddcc06097f48817fa4593f36c02dc24c
Beccatoni <rbccmtni1@gmail.com>
2025-02-20 19:35:10 +0200

    feat: create trip card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
       <h1 className="text-center font-bold text-4xl">Experience Rwanda</h1>
     </div>
   );
