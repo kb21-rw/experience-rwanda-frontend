79ad1c09a2cd59f56bcb13f15ba8519e89a39b26
AdelineA <adelinemug6@gmail.com>
2025-02-25 14:12:29 +0200

    feat: add styling

--- src/app/Component/ImageCardGrid/Card.tsx
+++ src/app/Component/ImageCardGrid/Card.tsx
@@ -7,17 +7,27 @@ const ImageCard = ({ place, image, price, date }: Card): ReactElement => {
 
   console.log("object.........................", url);
   return (
-    <div>
-      <Image
-        src={"/uploads/akagera.png"}
-        alt={alternativeText || "image"}
-        width={width}
-        height={height}
-      />
-      <div className=" ">
-        <h1>{place}</h1>
-        <p>{price}</p>
-        <p>{date}</p>
+    <div className="bg-white shadow rounded-3xl hover:border-2 hover:border-gray-400">
+      <div className="pt-3 flex justify-center">
+        <Image
+          src={"/uploads/akagera.png"}
+          alt={alternativeText || "image"}
+          width={width}
+          height={height}
+        />
+      </div>
+      <div className="p-6 flex flex-col gap-5">
+        <h1 className="font-bold text-xl font-figtree">Place: {place}</h1>
+        <p className="font-medium text-base">Price: {price}/day</p>
+        <p className="font-normal text-base">Date: {date}</p>
+
+        {/* Buttons soon will be replaced but button component */}
+        <button className="border border-gray-400 rounded-lg w-1/2 py-1">
+          More Details
+        </button>
+        <button className="bg-black py-2 text-white rounded-full pt-1">
+          Book Now
+        </button>
       </div>
     </div>
   );
--- src/app/Component/ImageCardGrid/index.tsx
+++ src/app/Component/ImageCardGrid/index.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from "react";
+import React from "react";
 import ImageCard from "./Card";
 import { Row } from "@/types/ImageCard";
 
@@ -8,15 +9,33 @@ const ImageCardGrid = ({
   cards,
 }: Omit<Row, "id" | "__component">): ReactElement => {
   return (
-    <section className="px-28">
-      <div className="flex flex-col items-center py-10 text-center">
-        <h1 className="font-bold text-5xl mb-5 ">{title}</h1>
-        <p>{description}</p>
+    <section className="bg-gray-100 px-28 py-[100px]">
+      <div className="flex flex-col items-center text-center">
+        <div className="flex flex-col gap-6">
+          <h1 className="font-bold text-5xl">{title}</h1>
+          <p className="text-xl">{description}</p>
+        </div>
+        <div className="flex gap-5 py-10 pb-5">
+          <button className="border border-t-2 border-gray-700 rounded-lg px-3 py-2 hover:bg-black hover:text-white">
+            All trips
+          </button>
+          <button className="border border-t-2 border-gray-700 rounded-lg px-3 py-2 hover:bg-black hover:text-white">
+            Nyungwe
+          </button>
+          <button className="border border-t-2 border-gray-700 rounded-lg px-3 py-2 hover:bg-black hover:text-white">
+            Akagera
+          </button>
+        </div>
       </div>
-      <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 xl:gap-10 gap-7.5">
+      <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 mt-10">
         {cards.map((data) => (
           <ImageCard key={data.id} {...data} />
         ))}
+      </div>
+      <div className="flex justify-center mt-[84px]">
+        <button className="border border-t-2 font-bold border-gray-700 rounded-lg px-3 py-2 hover:bg-black hover:text-white">
+          See More
+        </button>
       </div>
     </section>
   );
