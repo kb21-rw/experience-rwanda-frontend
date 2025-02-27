0c4f0fcb1c4cb012f113d9ed89022bd36dac1381
DevRamona <r.ingabire@alustudent.com>
2025-02-27 10:22:23 +0200

    refactor: change the input file location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
         <Button variant={"default"}>Book Now</Button>
         <Button variant={"secondary"}>Cancel</Button>
       </div>
+      
     </div>
   );
 }
