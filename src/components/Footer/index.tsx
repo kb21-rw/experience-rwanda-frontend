dae335a8869ed20de2879173a74ee4958f0d8ea4
Beccatoni <rbccmtni1@gmail.com>
2025-02-28 09:05:53 +0200

    chore: place data in their files

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,25 +1,27 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import Instagram from "@/app/assets/Instagram.svg";
-import Twitter from "@/app/assets/Twitter.svg";
-import Youtube from "@/app/assets/Youtube.svg";
-import { FooterData, IconName } from "@/types/footer";
+import { FooterData, SiteConfig } from "@/types/footer";
+import Image from "next/image";
 
 const Footer = () => {
-  const [data, setData] = useState<FooterData>({ logo: "", footerLinks: [], footerIcons: [] });
+  const [data, setData] = useState<FooterData>({
+    logo: "",
+    footerLinks: [],
+    footerIcons: [],
+  });
 
   useEffect(() => {
-    fetch("/data/data.json")
+    fetch("/data/siteConfig.json")
       .then((response) => response.json())
-      .then((data) => setData(data));
+      .then((config: SiteConfig) => setData(config.data.attributes.footer));
   }, []);
 
-  const iconComponents: Record<IconName, React.ComponentType> = {
-    instagram: Instagram,
-    twitter: Twitter,
-    youtube: Youtube,
-  };
+  // const iconCards: Record<IconName, React.ComponentType> = {
+  //   instagram: Instagram,
+  //   twitter: Twitter,
+  //   youtube: Youtube,
+  // };
 
   return (
     <footer className="absolute bottom-0 w-full flex justify-between items-center p-3 text-sm md:p-10 bg-black text-white mb-0">
@@ -27,17 +29,23 @@ const Footer = () => {
 
       <div className="navigation flex flex-col md:flex-row gap-3">
         {data.footerLinks.map((link) => (
-          <Link href={"#"} key={link}>
-            {link}
+          <Link href={"#"} key={link.id}>
+            {link.name}
           </Link>
         ))}
       </div>
 
       <div className="social-icons flex flex-col md:flex-row md:gap-3">
-        {data.footerIcons.map((icon) => {
-          const IconComponent = iconComponents[icon.name];
-          return <IconComponent key={icon.name} />;
-        })}
+        {data.footerIcons.map((icon) => (<a href={icon.url} key={icon.title}>
+            {icon.icon.data.attributes.url && (
+              <Image
+                src={icon.icon.data.attributes.url}
+                alt={icon.icon.data.attributes.alternativeText || icon.title}
+                width={icon.icon.data.attributes.width}
+                height={icon.icon.data.attributes.height}
+              />
+            )}
+          </a>))}
       </div>
     </footer>
   );
--- src/types/footer.ts
+++ src/types/footer.ts
@@ -1,12 +1,48 @@
-
 export type IconName = "instagram" | "twitter" | "youtube";
 
-export type FooterIcon = {
-    name: IconName;
-}
-
-export type FooterData = {
+export type IconAttributes = {
+    name: string;
+    alternativeText: string | null;
+    url: string;
+    width: number;
+    height: number;
+  };
+  
+  export type IconData = {
+    id: number;
+    attributes: IconAttributes;
+  };
+  
+  export type FooterIcon = {
+    title: IconName
+    url: string;
+    icon: {
+      data: IconData;
+    };
+  };
+  
+  export type FooterLink = {
+    id: number;
+    name: string;
+    url: string;
+  };
+  
+  export type FooterData = {
     logo: string;
-    footerLinks: string[];
+    footerLinks: FooterLink[];
     footerIcons: FooterIcon[];
-}
+  };
+  
+  export type SiteConfig = {
+    data: {
+      id: number;
+      attributes: {
+        createdAt: string;
+        updatedAt: string;
+        publishedAt: string;
+        locale: string;
+        footer: FooterData;
+      };
+    };
+    meta: Record<string, unknown>;
+  };
