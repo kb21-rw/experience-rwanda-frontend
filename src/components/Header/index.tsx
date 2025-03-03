d62c2c6940462f3946e4afd70f0e9b09b4981255
Beccatoni <rbccmtni1@gmail.com>
2025-03-03 12:18:52 +0200

    chore: change the json files to be ts files and make data to be normal objects

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,23 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import HeaderContent from './HeaderContent'
-import { HeaderProps } from '@/app/types/Header.types';
+// import { HeaderProps } from '@/app/types/Header.types';
+import { aboutUsData } from '../../../public/data/header';
 
 
 
 const Header = () => {
-    const [headerData, setHeaderData] = useState<HeaderProps>({title:"", description:""});
+    // const [headerData, setHeaderData] = useState<HeaderProps>({title:"", description:""});
 
-    useEffect(()=>{
-        fetch("/data/header.json")
-        .then(response=> response.json())
-        .then(data=> setHeaderData(data));
-    }, []);
+    // useEffect(()=>{
+    //     fetch("/data/header.json")
+    //     .then(response=> response.json())
+    //     .then(data=> setHeaderData(data));
+    // }, []);
+    const {title, description} = aboutUsData
   return (
     <section className='px-20'>
-        <HeaderContent title={headerData.title} description={headerData.description}/>
+        <HeaderContent title={title} description={description}/>
     </section>
   )
 }
