92381d59b44a5e39c5d5bb4b22b96f6afdd18752
Beccatoni <rbccmtni1@gmail.com>
2025-03-03 10:17:29 +0200

    chore: destructure properties of icon attributes to avoid repetition

--- src/components/Footer/Icons.tsx
+++ src/components/Footer/Icons.tsx
@@ -1,34 +1,44 @@
-"use client"
+"use client";
 
-import { FooterIcon, SiteConfig } from '@/types/footer'
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
-
-
+import { FooterIcon, SiteConfig } from "@/types/footer";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useMemo, useState } from "react";
 
 const Icons = () => {
   const [icons, setIcons] = useState<FooterIcon[]>([]);
 
- useEffect(() => {
-        fetch("/data/siteConfig.json")
-          .then((response) => response.json())
-          .then((config: SiteConfig) => setIcons(config.data.attributes.footer.footerIcons));
-      }, []);
+  useEffect(() => {
+    fetch("/data/siteConfig.json")
+      .then((response) => response.json())
+      .then((config: SiteConfig) =>
+        setIcons(config.data.attributes.footer.footerIcons)
+      );
+  }, []);
+
+  const socialIcons = useMemo(() => {
+    return icons.map((icon) => {
+      const { url, alternativeText, width, height } = icon.icon.data.attributes;
+      return (
+        <Link href={icon.url} key={icon.title}>
+          {url && (
+            <Image
+              src={url}
+              alt={alternativeText || icon.title}
+              width={width}
+              height={height}
+            />
+          )}
+        </Link>
+      );
+    });
+  }, [icons]);
+
   return (
     <div className="social-icons flex flex-col md:flex-row md:gap-3">
-            {icons.map((icon) => (<Link href={icon.url} key={icon.title}>
-                {icon.icon.data.attributes.url && (
-                  <Image
-                    src={icon.icon.data.attributes.url}
-                    alt={icon.icon.data.attributes.alternativeText || icon.title}
-                    width={icon.icon.data.attributes.width}
-                    height={icon.icon.data.attributes.height}
-                  />
-                )}
-              </Link>))}
-          </div>
-  )
-}
+      {socialIcons}
+    </div>
+  );
+};
 
-export default Icons
+export default Icons;
