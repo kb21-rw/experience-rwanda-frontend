32a7a5f9d641bc012d9c75ef0a34a3d8b107ec66
Beccatoni <rbccmtni1@gmail.com>
2025-03-05 12:13:31 +0200

    chore: remove empty lines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,13 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-   
-      
     <div>
       <div className="flex items-center justify-center">
-        <h1 className="text-center font-bold text-4xl">Experience Rwanda</h1>
+        {/* <h1 className="text-center font-bold text-4xl">Experience Rwanda</h1> */}
       </div>
-      <section id="home">
-        {/* Home content */}
-      </section>
-      <section id="bookings">
-        {/* Bookings content */}
-      </section>
-      <section id="about">
-        {/* About content */}
-      </section>
+      <section id="home">{/* Home content */}</section>
+      <section id="bookings">{/* Bookings content */}</section>
+      <section id="about">{/* About content */}</section>
       <HeroContent
         imageUrl={heroData.imageUrl}
         content={{ title: heroData.title, description: heroData.description }}
