cac55d65fbc457db81b4b7c9f426966a7d7046e4
Nestor Ngabonziza <ngabonest@gmail.com>
2025-03-17 10:12:15 +0200

    feat: making the hero image fill the screen

--- src/components/HeroContent/index.tsx
+++ src/components/HeroContent/index.tsx
@@ -5,15 +5,9 @@ import Content from "./Content";
 
 const HeroContent = ({ imageUrl, content }: Omit<HeroContent, "id">) => {
   return (
-    <section className="w-full h-screen">
-      <div className="absolute -z-50 inset-0">
-        <Image
-          src={imageUrl}
-          alt={"backgroundImage"}
-          layout="responsive"
-          width={1000}
-          height={1000}
-        />
+    <section className="w-full h-[calc(100vh-124px)] bg-red-500 bg-opacity-50">
+      <div className="h-full">
+        <Image src={imageUrl} alt={"backgroundImage"} fill />
       </div>
       <div className="content-wrapper">
         <div className="md:w-1/2 w-full xl:pt-25 md:pt-12.5 pt-2">
