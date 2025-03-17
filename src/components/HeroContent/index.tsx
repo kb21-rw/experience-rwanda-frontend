8065ff19f2d7ff245aba9cd3ea62ab472f61d233
Nestor Ngabonziza <ngabonest@gmail.com>
2025-03-17 15:57:57 +0200

    fix: hero image on mobile

--- src/components/HeroContent/index.tsx
+++ src/components/HeroContent/index.tsx
@@ -5,8 +5,15 @@ import Content from "./Content";
 
 const HeroContent = ({ imageUrl, content }: Omit<HeroContent, "id">) => {
   return (
-    <section className="w-full h-[calc(100vh-124px)]">
-      <Image src={imageUrl} alt={"backgroundImage"} fill />
+    <section className="w-full h-[calc(50vh-124px)] lg:h-[calc(100vh-124px)]">
+      <div className=" h-[calc(50vh-124px)] lg:h-[calc(100vh-124px)] w-full absolute -z-10">
+        <Image
+          className=" h-[calc(50vh-124px)] lg:h-[calc(100vh-124px)] object-cover"
+          src={imageUrl}
+          alt={"backgroundImage"}
+          layout="fill"
+        />
+      </div>
       <div className="content-wrapper">
         <div className="md:w-1/2 w-full xl:pt-25 md:pt-12.5 pt-2">
           <Content title={content.title} description={content.description} />
