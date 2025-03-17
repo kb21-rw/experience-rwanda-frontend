4e0901e6799a527b4f344a6a7f85424e08c48f29
Nestor Ngabonziza <ngabonest@gmail.com>
2025-03-17 14:55:13 +0200

    fix: removing unnecessary div

--- src/components/HeroContent/index.tsx
+++ src/components/HeroContent/index.tsx
@@ -5,10 +5,8 @@ import Content from "./Content";
 
 const HeroContent = ({ imageUrl, content }: Omit<HeroContent, "id">) => {
   return (
-    <section className="w-full h-[calc(100vh-124px)] bg-red-500 bg-opacity-50">
-      <div className="h-full">
-        <Image src={imageUrl} alt={"backgroundImage"} fill />
-      </div>
+    <section className="w-full h-[calc(100vh-124px)]">
+      <Image src={imageUrl} alt={"backgroundImage"} fill />
       <div className="content-wrapper">
         <div className="md:w-1/2 w-full xl:pt-25 md:pt-12.5 pt-2">
           <Content title={content.title} description={content.description} />
