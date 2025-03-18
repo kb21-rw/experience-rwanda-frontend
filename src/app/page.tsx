d2bd5e6f49bc6d4d4d89bfbda0515c092584de43
Nestor Ngabonziza <ngabonest@gmail.com>
2025-03-18 15:40:34 +0200

    feat: adding null variable in suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Toastify from "@/components/Toastify";
 import { Suspense } from "react";
 export default function Home() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={null}>
       <Toastify />
 
       <HeroContent
