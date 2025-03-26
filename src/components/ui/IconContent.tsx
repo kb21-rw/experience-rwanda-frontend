2d1e137e770e5b8108ef95a5d4bf71d351334c55
DevRamona <r.ingabire@alustudent.com>
2025-03-26 21:44:36 +0200

    refactor:implement requested changes

--- src/components/ui/IconContent.tsx
+++ src/components/ui/IconContent.tsx
@@ -1,8 +1,16 @@
 import React, {FC} from 'react'
-import IconContentProps from '@/types/IconContent.types'
+import { ReactNode } from "react";
+import { IconType } from "react-icons";
+
+type IconContentProps = {
+  icon: IconType;
+  content: ReactNode;
+  className?: string;
+};
+
 const IconContent:FC<IconContentProps> = ({icon:Icon, content, className}) => {
   return (
-    <div className={`flex items-center ${className}`}>
+    <div className={`flex items-center gap-1 ${className}`}>
         <Icon className= "text-xl"/>
         <span className='text-lg font-inter font-normal'/>{content}<span/>
 
