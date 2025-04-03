5e1b2e49cbd8eba37a1f246de7d1f5c067278967
Nestor Ngabonziza <ngabonest@gmail.com>
2025-04-03 11:18:03 +0200

    adding custom styles in tailwind config

--- src/components/TripPackage/Card.tsx
+++ src/components/TripPackage/Card.tsx
@@ -3,19 +3,19 @@ import Image, { StaticImageData } from "next/image";
 import { FaRegCheckCircle } from "react-icons/fa";
 import IconContent from "../ui/IconContent";
 
-interface TripPackageProps {
+type TripPackageProps = {
   title: string;
   icon: StaticImageData;
   items: string[];
-}
+};
 
 const TripPackageCard = ({ title, icon, items }: TripPackageProps) => {
   return (
-    <div className="bg-[#383838] w-[360px] bg-opacity-50 flex items-center flex-col rounded-[20px] py-6 text-white">
+    <div className="bg-gray-750 w-80 bg-opacity-50 flex items-center flex-col rounded-5 py-6 text-white">
       <div className="bg-white h-32 w-32 mb-16 mt-6 rounded-full flex items-center justify-center">
         <Image src={icon} alt={title} layout="raw" />
       </div>
-      <h3 className="mb-5 font-semibold text-[20px]">{title}</h3>
+      <h3 className="mb-5 font-semibold text-xl">{title}</h3>
       <ul>
         {items.map((item, index) => (
           <IconContent
