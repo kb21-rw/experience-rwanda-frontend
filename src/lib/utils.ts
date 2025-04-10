6fb7ee80a44ba1df6f42c4e7e96cf5acce4f5f02
Nestor Ngabonziza <ngabonest@gmail.com>
2025-04-10 10:17:03 +0200

    refactor: removing unnecessary code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,6 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-export function isNumeric(str: string) {
-  return !isNaN(Number(str)) && !isNaN(parseFloat(str));
-}
