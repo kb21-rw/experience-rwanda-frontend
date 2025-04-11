5af98cb60be76ef28dcd65ba086e35da1bae6d7e
Peninah98 <peninangizwenayo@gmail.com>
2025-04-11 10:45:48 +0200

    feat: make footer stick on the bottom

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,16 @@
-import FooterLinks from "./FooterLinks";
-import { footerData } from "../../data/siteConfig";
-import FooterIcons from "./Icons";
+import FooterLinks from "./FooterLinks"
+import { footerData } from "../../data/siteConfig"
+import FooterIcons from "./Icons"
 
 const Footer = () => {
-const {logo} = footerData;
+  const { logo } = footerData
   return (
-    <footer className="footer w-full flex flex-col gap-4 md:flex-row justify-between items-center p-5 text-sm md:p-10 bg-black text-white mb-0">
+    <footer className="footer w-full flex flex-col gap-4 md:flex-row justify-between items-center p-5 text-sm md:p-10 bg-black text-white mb-0 mt-auto">
       <h3 className="text-2xl font-bold md:text-base ">{logo}</h3>
-      <FooterLinks/>
-      <FooterIcons/>
+      <FooterLinks />
+      <FooterIcons />
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
