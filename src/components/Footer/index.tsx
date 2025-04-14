d331c3a99e0a138eb2c6626372056e4b43019738
Peninah98 <peninangizwenayo@gmail.com>
2025-04-14 10:15:53 +0200

    refactor: remove unused class in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ import FooterIcons from "./Icons"
 const Footer = () => {
   const { logo } = footerData
   return (
-    <footer className="footer w-full flex flex-col gap-4 md:flex-row justify-between items-center p-5 text-sm md:p-10 bg-black text-white mb-0 mt-auto">
+    <footer className="w-full flex flex-col gap-4 md:flex-row justify-between items-center p-5 text-sm md:p-10 bg-black text-white mb-0 mt-auto">
       <h3 className="text-2xl font-bold md:text-base ">{logo}</h3>
       <FooterLinks />
       <FooterIcons />
