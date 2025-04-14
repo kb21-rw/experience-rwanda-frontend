df78ea2908c9a2955d66dc95f806e0a8b46d4999
Peninah98 <peninangizwenayo@gmail.com>
2025-04-14 12:09:28 +0200

    fix: fix details page

--- src/components/ui/IconContent.tsx
+++ src/components/ui/IconContent.tsx
@@ -1,12 +1,11 @@
-import React, { FC } from "react";
-import { ReactNode } from "react";
-import { IconType } from "react-icons";
+import { FC, ReactNode } from "react"
+import { IconType } from "react-icons"
 
 type IconContentProps = {
-  icon: IconType;
-  content: ReactNode;
-  className?: string;
-};
+  icon: IconType
+  content: ReactNode
+  className?: string
+}
 
 const IconContent: FC<IconContentProps> = ({
   icon: Icon,
@@ -15,10 +14,17 @@ const IconContent: FC<IconContentProps> = ({
 }) => {
   return (
     <div className={`flex items-center gap-3 ${className}`}>
-      <Icon className="text-lg" />
-      <span className="text-sm font-inter font-normal">{content}</span>
+      <div className="bg-black p-3 rounded-full">
+        <Icon
+          className="text-white text-xl"
+          size={20}
+        />
+      </div>
+      <span className="text-md font-inter font-normal text-black">
+        {content}
+      </span>
     </div>
-  );
-};
+  )
+}
 
-export default IconContent;
+export default IconContent
