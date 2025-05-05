4ca87b02238a00b014a27f7d9621cc4ee885aefe
DevRamona <r.ingabire@alustudent.com>
2025-05-05 08:56:44 +0200

    refactor: refactor the login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "./Form";
+
+export default function LoginPage() {
+    return (
+        <main className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center px-4 style = {{backgroundImage: `url('/uploads/hand.png')`}}">	
+       <LoginForm />
+        </main>
+    )
+}
+
