86ce0c5ced09a0c8ef86d6f5a9be53b653f63528
Nestor Ngabonziza <ngabonest@gmail.com>
2025-08-13 20:32:39 +0200

    feat: adding the currency

--- src/components/ImageCardGrid/PriceTag.tsx
+++ src/components/ImageCardGrid/PriceTag.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-const PriceTag = ({ price }: { price: number }) => {
+const PriceTag = ({ price, currency }: { price: number; currency: string }) => {
   return (
     <div className="flex flex-col gap-1 text-base leading-[100%]">
       <div className="flex gap-2 font-semibold">
-        <span>From</span> <span>{price} Rwf</span>
+        <span>From</span>{" "}
+        <span>
+          {price} {currency}
+        </span>
       </div>
       <span className="font-bold text-[#FFA500]">Per person</span>
     </div>
