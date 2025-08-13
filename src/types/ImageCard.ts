3b14d9d74aa64fb99255dd61db2d3e0547913a9d
Nestor Ngabonziza <ngabonest@gmail.com>
2025-08-13 19:30:46 +0200

    feat: updating the type of image card

--- src/types/ImageCard.ts
+++ src/types/ImageCard.ts
@@ -1,16 +1,24 @@
+export type PricingOption = {
+  id: string;
+  name: string;
+  amount: number;
+  description: string;
+};
+
 export type Card = {
   id: string;
   title: string;
   description?: string;
   trip: string;
   departureTime: string;
-  price: number | string;
+  price: number;
   seats: number;
-  seatsBooked?: number;
+  seatsBooked: number;
   currency?: string;
   mainPicture: string;
   createdAt?: string;
   updatedAt?: string;
+  pricingOptions?: PricingOption[];
 };
 
 export type Row = {
