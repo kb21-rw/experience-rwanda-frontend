13b245c357c0370a3ad181797f3cefd3fb7a0570
Nestor Ngabonziza <ngabonest@gmail.com>
2025-08-13 19:28:26 +0200

    feat: removing the price in icon content in trip card

--- src/data/tripDetails.ts
+++ src/data/tripDetails.ts
@@ -1,5 +1,4 @@
 import { IoLocationSharp } from "react-icons/io5";
-import { GiPriceTag } from "react-icons/gi";
 import { MdAirlineSeatReclineExtra } from "react-icons/md";
 import { FaCalendar } from "react-icons/fa";
 import { format } from "date-fns";
@@ -7,7 +6,6 @@ import { format } from "date-fns";
 export const createTripDetails = (
   location: string,
   date: string,
-  price: number | string,
   seats: number
 ) => [
   {
@@ -17,10 +15,6 @@ export const createTripDetails = (
   {
     icon: FaCalendar,
     content: format(new Date(date), "MMMM dd, yyyy"),
-  },
-  {
-    icon: GiPriceTag,
-    content: `${price}$ / day`,
   },
   {
     icon: MdAirlineSeatReclineExtra,
