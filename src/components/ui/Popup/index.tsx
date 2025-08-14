8239a0f44b84b8ed568b3563e32785d8e5802150
Nestor Ngabonziza <ngabonest@gmail.com>
2025-08-14 10:22:50 +0200

    fix: pricingoption types

--- src/components/ui/Popup/index.tsx
+++ src/components/ui/Popup/index.tsx
@@ -2,7 +2,7 @@ import React, { Dispatch, SetStateAction, useState } from "react";
 import UserInfo from "./UserInfo";
 import PaymentPopup from "./Payment";
 import { ClientData } from "@/types/Popup";
-import { PricingOption } from "@/types/trip";
+import { PricingOption } from "@/types/ImageCard";
 
 const Popup = ({
   tripId,
