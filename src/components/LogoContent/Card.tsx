9bfaebce371afbfb0d53793b12725dc21c648d7f
kareraolivier <kareraolivier@gmail.com>
2025-08-18 10:54:22 +0200

    feat: add text in center

--- src/components/LogoContent/Card.tsx
+++ src/components/LogoContent/Card.tsx
@@ -4,10 +4,10 @@ import { LogoCard } from "@/types/LogoContent";
 
 const Card = ({ title, description, image }: LogoCard) => {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 items-center md:items-start">
       <Image src={image} alt={image} width={48} height={48} />
       <h3 className="font-bold text-3xl">{title}</h3>
-      <p>{description}</p>
+      <p className="text-center md:text-left">{description}</p>
     </div>
   );
 };
--- src/components/LogoContent/index.tsx
+++ src/components/LogoContent/index.tsx
@@ -12,7 +12,7 @@ const LogoContent = ({ title, description, cards }: LogoContent) => {
           </h1>
           <p className="text-base font-normal font-inter">{description}</p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {cards.map((card: LogoCard) => (
             <Card key={card.id} {...card} />
           ))}
