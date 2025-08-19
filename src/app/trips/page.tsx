518598bd518ce17090ad6a195d974d8a1d2dc293
kareraolivier <kareraolivier@gmail.com>
2025-08-19 14:47:05 +0200

    feat: add trips page header

--- src/app/trips/page.tsx
+++ src/app/trips/page.tsx
@@ -2,15 +2,19 @@ import Header from "@/components/Header";
 import { HeaderVariant } from "@/enums/Header";
 import React from "react";
 
+const header = {
+  title: "Book trip by ExperienceRW",
+  description:
+    "Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum",
+};
+
 const TripsPage = () => {
   return (
     <section className="  bg-site text-white min-h-screen">
       <div className="content-wrapper">
         <Header
-          title="Book trip by ExperienceRW"
-          description={
-            "Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum"
-          }
+          title={header.title}
+          description={header.description}
           variant={HeaderVariant.PRIMARY}
         />
       </div>
