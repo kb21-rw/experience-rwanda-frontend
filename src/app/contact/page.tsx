e91fd2286125354b224d79d47808dfaa2d904a35
kareraolivier <kareraolivier@gmail.com>
2025-08-20 21:51:14 +0200

    feat: add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,44 @@
+import Image from "next/image";
 import ContactForm from "./Form";
 
 const ContactPage = () => {
   return (
-    <div className="py-25">
-      <h1 className="text-10 text-center font-bold font-inter">Contact Us</h1>
-      <ContactForm />
-    </div>
+    <section className="py-25 lg:bg-site  relative">
+      <div className="content-wrapper grid grid-cols-1 lg:grid-cols-2">
+        <div className="relative z-10">
+          <ContactForm />
+        </div>
+
+        <div className="relative hidden lg:block">
+          <div className="w-3/4 h-full place-self-end">
+            <Image
+              src="/uploads/contactone.jpg"
+              alt="contact"
+              width={1000}
+              height={1000}
+              className="object-cover w-full h-full"
+            />
+          </div>
+          <Image
+            src="/uploads/contacttwo.png"
+            alt="contact"
+            width={300}
+            height={300}
+            className="object-cover absolute top-1/2 -translate-y-1/2 left-0 w-1/2 h-3/4"
+          />
+        </div>
+      </div>
+
+      <div className="absolute inset-0 lg:hidden -z-10">
+        <Image
+          src="/uploads/contactone.jpg"
+          alt="contact"
+          fill
+          className="object-cover w-full h-full"
+          priority
+        />
+      </div>
+    </section>
   );
 };
 
