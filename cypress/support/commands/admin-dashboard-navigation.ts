536ea9ddf9af7c6bc8cfb13dde898dff906d429b
Ritha-Ven <rithakankindi@gmail.com>
2025-08-27 16:29:48 +0200

    chore:added missing url

--- cypress/support/commands/admin-dashboard-navigation.ts
+++ cypress/support/commands/admin-dashboard-navigation.ts
@@ -2,6 +2,7 @@ import { Step } from "../types";
 
 Cypress.Commands.add("adminDashboardNavigation", function () {
   cy.fixture("admin-dashboard-navigation.json").then((data: Step[]) => {
+    cy.visit("/login?redirect=/admin");
     data.forEach((step) => {
       switch (step.action) {
         case "press":
