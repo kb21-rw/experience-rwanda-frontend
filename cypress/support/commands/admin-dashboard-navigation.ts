413ed02110a53fa7558f9fd8bcef7adb57db569c
Ritha-Ven <rithakankindi@gmail.com>
2025-08-28 10:46:38 +0200

    chore:modified admin dadshboard url

--- cypress/support/commands/admin-dashboard-navigation.ts
+++ cypress/support/commands/admin-dashboard-navigation.ts
@@ -2,7 +2,7 @@ import { Step } from "../types";
 
 Cypress.Commands.add("adminDashboardNavigation", function () {
   cy.fixture("admin-dashboard-navigation.json").then((data: Step[]) => {
-    cy.visit("/login?redirect=/admin");
+    cy.visit("http://localhost:3000/login?redirect=/admin");
     data.forEach((step) => {
       switch (step.action) {
         case "type":
